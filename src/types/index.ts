12f38dac802664e4a21cfef58b6da3aa76363713
AariF-ShazZ <aarif568khan@gmail.com>
2026-05-31 22:38:31 +0530

    work in progress, date - 31/05/2026

---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+
+export interface Category {
+  id: number;
+  name: string;
+  image: string;
+}
+
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: Category;
+  images: string[];
+}
+
+export interface ProductsApiParams {
+  categoryId?: number;
+  title?: string;
+  price_min?: number;
+  price_max?: number;
+  offset?: number;
+  limit?: number;
+}
+
+export interface CartItem {
+  product: Product;
+  quantity: number;
+}
